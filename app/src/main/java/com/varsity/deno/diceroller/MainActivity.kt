39defbcbd6f478e46193df503220cbfab15403bb
Denis Muthomi <muthomidenis62@gmail.com>
2019-05-29 11:12:00 +0300

    dice

--- app/src/main/java/com/varsity/deno/diceroller/MainActivity.kt
+++ app/src/main/java/com/varsity/deno/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.varsity.deno.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
 
@@ -13,25 +14,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's Roll"
-
         rollButton.setOnClickListener {
-            rollButton.setOnClickListener {
-                rollDice()
-            }
+            rollDice()
+
 
         }
     }
 
     private fun rollDice() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-
         val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        resultText.text = randomInt.toString()
-
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
 
